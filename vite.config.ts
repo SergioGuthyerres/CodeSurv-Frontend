e2b6a218721aff850d2896c79bef610146860bdf
Sergio Guthyerres dos Santos de Morais <hyp@Sergios-MacBook-Air.local>
2026-03-19 22:55:59 -0300

    Adicionado conexão socket-client

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,17 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    proxy: {
+      "/socket.io": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
